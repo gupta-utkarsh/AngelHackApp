534c89d4071afd53805ca5f3829b2888919ff7cb
Mohan Agrawal <mohan.agr97@gmail.com>
2016-07-09 16:47:38 +0530

    Initial auth support

---
+++ app/Doctor.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Doctor extends Authenticatable
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    protected $guard = 'doctors';
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,40 @@
 |
 */
 
+Route::group(['middleware' => ['web']], function(){
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Authentication Routes...
+Route::get('login', 'Auth\AuthController@showLoginForm');
+Route::post('login', 'Auth\AuthController@login');
+Route::get('logout', 'Auth\AuthController@logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\AuthController@showRegistrationForm');
+Route::post('register', 'Auth\AuthController@register');
+
+// Password Reset Routes...
+Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
+Route::post('password/email', 'Auth\PasswordController@sendResetLinkEmail');
+Route::post('password/reset', 'Auth\PasswordController@reset');
+
+Route::get('doclogin', 'DoctorAuth\AuthController@showLoginForm');
+Route::post('doclogin', 'DoctorAuth\AuthController@login');
+Route::get('doclogout', 'DoctorAuth\AuthController@logout');
+
+// Registration Routes...
+Route::get('docregister', 'DoctorAuth\AuthController@showRegistrationForm');
+Route::post('docregister', 'DoctorAuth\AuthController@register');
+
+// Password Reset Routes...
+Route::get('docpassword/reset/{token?}', 'DoctorAuth\PasswordController@showResetForm');
+Route::post('docpassword/email', 'DoctorAuth\PasswordController@sendResetLinkEmail');
+Route::post('docpassword/reset', 'DoctorAuth\PasswordController@reset');
+
+Route::get('/home', 'HomeController@index');
+
+});
+
